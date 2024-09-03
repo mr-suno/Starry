d0d4e4f681333344941306da2e982815a0beb9df
Suno <ilovesuno@proton.me>
2024-09-02 21:37:35 -0700

    Change URL

--- version/main.lua
+++ version/main.lua
@@ -20,10 +20,8 @@ local getName = string.lower(identifyexecutor())
 
 if string.find(getName, "celery") then
     localPlayer:Kick("Celery is not supported with Starry! Please use a different executor.")
-elseif string.find(getName, "delta") then
-    localPlayer:Kick("Delta is currently being fixed with Starry! Please use a different executor.")
 elseif string.find(getName, "appleware") then
     localPlayer:Kick("AppleWare is currently being fixed with Starry! Please use a different executor.")
 else
-    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/089e8f5b8ac259500a44ccd0537ab253.lua"))()
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/bca2afc1415c691595c16ff6cefde60f.lua"))()
 end
